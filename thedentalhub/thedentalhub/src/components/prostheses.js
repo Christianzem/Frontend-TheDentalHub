80b2a1177bf95d470c248d9cc7461283e32cbe4a
Christian Zempoalteca <bunny_benito@Christians-MacBook-Air.local>
2024-05-30 08:59:34 -0700

    Removed duplicate

--- thedentalhub/thedentalhub/src/components/prostheses.js
+++
@@ -1,8 +0,0 @@
-// This will contain all prothesis infomation and be the child of patientDetials
-
-const { useState } = require("react")
-
-const Prostheses = () => {
-    const [prostheses, setProstheses] = useState([]) 
-
-}
